57e62f2506d6ca37df931d79b15093f47866b6b6
Priyanka-Prajapati <contact.priyankaprajapati@gmail.com>
2021-05-12 15:17:29 +0530

    stylesheet added

--- app.js
+++ app.js
@@ -5,47 +5,35 @@ const btnTranslate = document.querySelector("#btn-translate");
 const outputTxt = document.querySelector("#output-txt");
 
 // API used for translation
-const serverURL= "https://api.funtranslations.com/translate/shakespeare.json";
+const serverURL = "https://api.funtranslations.com/translate/shakespeare.json";
 
 btnTranslate.addEventListener("click", clickEventHandler);
 
-function constructURL(text){
-    return serverURL+"?text="+text;
+function constructURL(text) {
+  return serverURL + "?text=" + text;
 }
- 
+
 //  function for error handling
 
-function errorHandler(error){
-     
-    console.log("error occured: " ,error);
-    alert("Something wrong with the server! Please try again after some time.")
-
- }
-
-
-
+function errorHandler(error) {
+  console.log("error occured: ", error);
+  alert("Something wrong with the server! Please try again after some time.");
+}
 
 // function for input-output processing
 
-function clickEventHandler(){
+function clickEventHandler() {
+  let inputDesc = inputTxt.value;
+  // console.log(inputDesc);
 
-    let inputDesc = inputTxt.value
-    // console.log(inputDesc);
+  // calling server for processing
 
-    // calling server for processing
-
-    fetch(constructURL(inputDesc))
-    .then(response => response.json())
-    .then(json => {
-          
-        let translatedText = json.contents.translated;
-         outputTxt.innnerText = translatedText;
-        //  console.log(json.contents.translated)
-
+  fetch(constructURL(inputDesc))
+    .then((response) => response.json())
+    .then((json) => {
+      let translatedText = json.contents.translated;
+      outputTxt.innerText = translatedText;
+      //  console.log(translatedText);
     })
-    .catch(erronHandler);
-
-
-
-
+    .catch(errorHandler);
 }
